ac2a9561a8989420735cce99c44a5dd617cdaac7
kaygo <rom213434@gmail.com>
2017-07-31 21:21:59 +0300

    -исправил прямоугольник и квадрат -теугольник пока в процессе

--- Figure.py
+++ Figure.py
@@ -31,49 +31,22 @@ class Triangle(Polygon):
 
 class IsoscelesTriangle(Triangle):
     def __init__(self, p1, p2, l):
-        a = (pow(p1.y, 2) - pow(p2.y, 2)) / (p1.y - p2.y)
-        b = p1.x + pow(pow(l, 2) - pow(a - p1.y, 2), 1/2)
-        if pow(b - p1.x, 2) - pow(a - p1.y, 2) == pow(b - p2.x, 2) - pow(a - p2.y, 2) == pow(l, 2):
-            p3 = Point(b, a)
-            super().__init__(p1, p2, p3)
-        elif pow(-1*b - p1.x, 2) - pow(a - p1.y, 2) == pow(-1*b - p2.x, 2) - pow(a - p2.y, 2) == pow(l, 2):
-            p3 = Point(-b, a)
-            super().__init__(p1, p2, p3)
-        elif pow(b - p1.x, 2) - pow(-1 * a - p1.y, 2) == pow(b - p2.x, 2) - pow(-1 * a - p2.y, 2) == pow(l, 2):
-            p3 = Point(b, -a)
-            super().__init__(p1, p2, p3)
-        elif pow(-1 * b - p1.x, 2) - pow(-1 * a - p1.y, 2) == pow(-1 * b - p2.x, 2) - pow(-1 * a - p2.y, 2) == pow(l, 2):
-            p3 = Point(-b, -a)
+        
             super().__init__(p1, p2, p3)
 
 
 class Rectangle(Polygon):
     def __init__(self, l_d, r_u):
-        a = 0
-        b = 0
-        if l_d.y < 0:
-            a = -1 * l_d.y
-        elif r_u.y < 0:
-            b = -1 * r_u.y
-        sum = a + b
-        c = l_d.x
-        d = l_d.y + sum
-        l_u = Point(c, d)
-        if l_d.x < 0:
-            a = -1 * l_d.x
-        elif r_u.x < 0:
-            b = -1 * r_u.x
-        sum = a + b
-        d = l_d.x + sum
-        c = l_d.y
-        r_d = Point(c, d)
+        l_u = Point(l_d.x, r_u.y)
+        r_d = Point(r_u.x, l_d.x)
         super().__init__(l_d, l_u, r_d, r_u)
 
 
 class Square(Rectangle):
     def __init__(self, p, l):
-        z = Point(p.x + l, p.y + l)
-        super().__init__(p, z)
+        l_d = p
+        r_u = Point(p.x + l, p.y + l)
+        super().__init__(l_d, r_u)
 
 
 
