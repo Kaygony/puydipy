f999e09053cce54e5b8f4fd20177a29c8a1fbad6
kaygo <rom213434@gmail.com>
2017-08-01 13:16:16 +0300

    -сделал треугольник

--- Figure.py
+++ Figure.py
@@ -31,8 +31,23 @@ class Triangle(Polygon):
 
 class IsoscelesTriangle(Triangle):
     def __init__(self, p1, p2, l):
-        
-            super().__init__(p1, p2, p3)
+        p2.x = p2.x - p1.x
+        p2.y = p2.y - p1.y
+        p1.x = 0
+        p1.y = 0
+        c = (-p2.x ** 2 - p2.y ** 2) / (-2)
+        if p2.x != 0:
+            a = p2.y ** 2 + p2.x ** 2
+            b = -2 * p2.y * c
+            e = c ** 2 - l ** 2 * p2.x ** 2
+            d = b ** 2 - 4 * a * e
+            m = (-b + d ** (1 / 2)) / (2 * a)
+            n = (c - m * p2.y) / p2.x
+        else:
+            m = c / p2.y
+            n = (l ** 2 - (c ** 2) / p2.y ** 2) ** (1 / 2)
+        p3 = Point(n, m)
+        super().__init__(p1, p2, p3)
 
 
 class Rectangle(Polygon):
